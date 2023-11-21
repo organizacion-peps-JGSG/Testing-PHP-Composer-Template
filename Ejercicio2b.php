a5861228b9f803c82c701a3084958f2158b70c6b
Javier Vinal <98608731+JavierVICO@users.noreply.github.com>
2023-11-21 22:42:26 +0100

    update Ejercicio2b.php

--- Ejercicio2b.php
+++ Ejercicio2b.php
@@ -1,43 +1,24 @@
 <?php
-function esMayorEdad($edad) {
-    return $edad >= 18;
+
+$nombre = strval(readline("Introduce tu nombre: "));
+
+$edad = intval(readline("Introduce tu edad: "));
+
+if (esMayorEdad($edad)) {
+    echo "Usted es mayor de edad";
+} else {
+    echo "Todavía eres menor de edad";
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Verificar si los campos 'nombre' y 'edad' están establecidos en $_POST
-    if (isset($_POST['nombre']) && isset($_POST['edad'])) {
-        $nombre = $_POST['nombre'];
-        $edad = $_POST['edad'];
 
-        // Verificar si los campos 'nombre' y 'edad' no están vacíos
-        if (!empty($nombre) && !empty($edad)) {
-            if (esMayorEdad($edad)) {
-                echo "Hola $nombre, puedes conducir.";
-            } else {
-                echo "Hola $nombre, todavía no puedes conducir.";
-            }
-        } else {
-            echo "Por favor, ingresa nombre y edad.";
-        }
-    } else {
-        echo "Por favor, ingresa nombre y edad.";
+
+function esMayorEdad($e)
+{
+    if ($e >= 18)
+    {
+        return true;
+    } else 
+    {
+        return false;
     }
 }
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form method="post">
-        <input type="text" name="nombre" alt="nombre" placeholder="Nombre">
-        <input type="number" name="edad" alt="edad" placeholder="Edad">
-        <button type="submit" name="submit">Comprobar</button>
-        <p></p>
-    </form>
-</body>
-</html>
